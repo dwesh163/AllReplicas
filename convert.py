309f69e9ef4e96bb7c134f6f6a0db3adf89da6f5
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-11-26 14:29:09 +0100

    [add] script to convert text to JSON format

---
+++ convert.py
@@ -0,0 +1,31 @@
+import json
+import codecs
+
+def transformFile(inputFile, outputFile):
+    with codecs.open(inputFile, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    print(lines)
+
+    results = []
+
+    for line in lines:
+        line = line.strip()
+
+        if line != "":
+            user_input = input(line + " : ")
+
+            results.append([line, int(user_input)])
+
+            with open(outputFile, 'w', encoding='utf-8') as json_file:
+                json.dump(results, json_file, indent=2, ensure_ascii=False)
+        
+
+
+
+if __name__ == "__main__":
+    inputFile = "text.txt"
+    outputFile = "resultats.json"
+
+    transformFile(inputFile, outputFile)
+
